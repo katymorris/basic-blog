8a0316ffc6b2a742e99da29b86b0287b18e3d5ec
Katy Morris <katymorris@Katys-MBP.home>
2016-10-16 19:50:58 -0700

    first commit

--- about.php
+++ about.php
@@ -13,11 +13,7 @@ Blog
 			<?php require("header.php") ?>
 			<img class="about-pic" src="http://localhost:8888/blog/wp-content/themes/blog/images/about-pic.jpg">
 			<div class="about-text">
-				<p>When I was building this blog I asked my husband what I should name it. He was tired and heading to bed and said "do not forget to put away the lentils." In our house, the last one upstairs puts away dinner leftovers. Of course, being me, I always forget - so he was reminding me.</p>
-
-				<p>I said "ok, but what should I name it?" He repeated "do not forget to put away the lentils." He was being silly and really just didn't feel like giving me ideas since he was off to bed, but I tried just one more time: "I will, I will. But what should I name it?", "Don't forget to put away the lentils," he replied. So I jokingly titled the blog just that and sent it to him. I kept it there for a while since I didn't have any other ideas anyway, and it at least filled up the header space.</p>
-
-				<p>I'm a very simple person. My 3 things stranded on an island would be a a lifetime supply of pasta (uncooked, assuming I would have means to cook it), tomato seeds so I can least have good sauce, and my macbook with satallite internet or something (whatever they use on isolated islands). It HAS to have the internet or I just go on that stranded island.</p>
+				
 				
 				<p></p>
 			</div>
